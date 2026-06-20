7be0583e51fb9329c7ffc45dd0d0273f334083fa
jackyfogel <jackyfogel@gmail.com>
2026-06-20 18:41:23 +0300

    ow

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth.models import User; u=User.objects.filter(is_superuser=True).first(); u.set_password('12341234'); u.save(); print(f'Reset password for {u.username}')" | python manage.py shell
